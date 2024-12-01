d63928730639c9a8589fcc2de9236c6750a27871
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 00:01:12 -0800

    green

--- src/view/CanvasView.ts
+++ src/view/CanvasView.ts
@@ -1,3 +1,14 @@
 export class CanvasView {
-    constructor(canvasName: string) {}
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D | null;
+    constructor(canvasName: string) {
+        this.canvas = document.querySelector(canvasName);
+        this.context = this.canvas?.getContext('2d') || null;
+    }
+
+    clear(): void{
+        if (this.context) {
+            this.context?.clearRect(0,0,this.canvas.width, this.canvas.height);
+        }
+    }
 }
