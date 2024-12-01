89e02c0085d77a2ee2efb0416825b1e5fadaa5b3
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 01:16:24 -0800

    green

--- src/sprites/Sprite.ts
+++ src/sprites/Sprite.ts
@@ -3,12 +3,14 @@ export class Sprite {
     private readonly _x: number;
     private readonly _y: number;
     private readonly _w: number;
+    private readonly _h: number;
 
     constructor(imageSrc: string, xCoord: number, yCoord: number, width: number, height: number = 0) {
         this._imageSrc = imageSrc;
         this._x = xCoord
         this._y = yCoord
         this._w = width;
+        this._h = height;
     }
 
     get imageSrc(): string{
@@ -25,5 +27,9 @@ export class Sprite {
 
     get width(): number{
         return this._w
+    }
+
+    get height(): number{
+        return this._h
     }
 }
