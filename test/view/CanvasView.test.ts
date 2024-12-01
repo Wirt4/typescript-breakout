4ec26c3fc28155be289ba1746baeb2aef6cc4bbc
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 00:59:23 -0800

    red

--- test/view/CanvasView.test.ts
+++ test/view/CanvasView.test.ts
@@ -181,4 +181,12 @@ describe('CanvasView.drawSprite',()=>{
         expect(mockContext.drawImage).toHaveBeenCalledWith(expect.objectContaining({src: expect.stringContaining('image3.png')}),
             expect.anything() ,expect.anything(), expect.anything(), expect.anything());
     })
+    it("when drawSprites is called, expect drawImage to be called with the sprite's X cord",()=>{
+        canvasView = new CanvasView('#playField');
+        const sprite = new Sprite('../../image3.png', 3)
+        canvasView.drawSprite(sprite)
+        expect(mockContext.drawImage).toHaveBeenCalledWith(expect.anything(),
+            3,
+            expect.anything(), expect.anything(), expect.anything());
+    })
 })
