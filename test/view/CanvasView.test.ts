37f5d289033b0437c7a2eece04ac656b07a86674
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-11-30 23:50:01 -0800

    green

--- test/view/CanvasView.test.ts
+++ test/view/CanvasView.test.ts
@@ -1,3 +1,5 @@
+import {CanvasView} from "../../src/view/CanvasView";
+
 describe('CanvasView constructor tests', () => {
     it('canvasview instantiates without error',()=>{
         const view = new CanvasView()
