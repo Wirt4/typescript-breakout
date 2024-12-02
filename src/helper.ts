c32a34f775f469bf019a21595bd7535c134b8f6e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 18:52:01 -0800

    green

--- src/helper.ts
+++ src/helper.ts
@@ -2,5 +2,5 @@ import {LEVEL} from "./setup";
 import {Brick} from "./sprites/Brick";
 
 export function createBricks(){
-    return LEVEL.map(()=> new Brick('stub', {x:0,y:0}, {width: 0, height: 0}));
+    return LEVEL.map(()=> new Brick('stub', {x:4,y:0}, {width: 0, height: 0}));
 }
