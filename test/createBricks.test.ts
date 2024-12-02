7b33e56eef9145bbd7f0178cb27fd788f4c261a7
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 18:30:34 -0800

    red

--- test/createBricks.test.ts
+++ test/createBricks.test.ts
@@ -9,4 +9,8 @@ describe('createBricks', () => {
     it('Given that the setup const LEVEL returns an empty array, when createBricks is called, then it returns an empty array',()=>{
         expect(createBricks()).toEqual([])
     })
+    it('Given that the setup const LEVEL returns an empty array, when createBricks is called, then it returns an array with length of one',()=>{
+        const bricks = createBricks()
+        expect(bricks.length).toEqual(1)
+    })
 })
