3b6cc812d0e79cc9f026de1e752ca77866c0e89c
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 18:05:53 -0800

    green

--- src/sprites/Brick.ts
+++ src/sprites/Brick.ts
@@ -1,13 +1,10 @@
 import {Sprite} from "./Sprite";
 
 export class Brick extends Sprite{
-    private readonly _energy: number;
+    public energy: number;
+
     constructor(imgSrc: string, xPos: number, yPos: number, width: number, height: number, energy: number = 1) {
         super(imgSrc, xPos, yPos, width, height);
-        this._energy = energy;
-    }
-
-    get energy(){
-        return this._energy;
+        this.energy = energy;
     }
 }
