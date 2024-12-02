5660dc60b6fca65d702f8b024ab1dc0214310fea
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 18:59:45 -0800

    green

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,10 @@
-import {LEVEL, STAGE_PADDING} from "./setup";
+import {BRICK_PADDING, BRICK_WIDTH, LEVEL, STAGE_PADDING} from "./setup";
 import {Brick} from "./sprites/Brick";
 
 export function createBricks(){
-    return LEVEL.map(()=> new Brick('stub', {x:STAGE_PADDING,y:0}, {width: 0, height: 0}));
+    return LEVEL.map((element, ndx)=> {
+        const xCoord=  STAGE_PADDING + ndx*(BRICK_WIDTH+BRICK_PADDING)
+        return  new Brick('stub', {x: xCoord, y:0}, {width: 0, height: 0})
+    });
 }
+
