dff806d778a8a9bc56dc50eb4ceec21498a623fe
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-01 18:01:01 -0800

    red

--- test/sprites/Brick.test.ts
+++ test/sprites/Brick.test.ts
@@ -2,7 +2,11 @@ import {Brick} from "../../src/sprites/Brick";
 
 describe('basic sprite getters', () => {
     it('brick width is 40',()=>{
-        const brick = new Brick('stub',0,0, 40, 10);
+        const brick = new Brick('stub',0, 0, 40, 10);
         expect(brick.width).toBe(40);
+    })
+    it('brick energy is 1',()=>{
+        const brick = new Brick('stub',0, 0, 40, 10);
+        expect(brick.energy).toBe(1);
     })
 })
