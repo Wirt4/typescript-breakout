2455e4630bb48ed7b94dd8d6c12bfdf4784ca2fe
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-02 13:13:58 -0800

    green

--- src/helper.ts
+++ src/helper.ts
@@ -18,7 +18,7 @@ export function createBricks(): Brick[] {
     return LEVEL.reduce((accumulated, element, ndx)=>{
         if (element <=0) return accumulated
         const stats = brickStats(element)
-        return [...accumulated,new Brick(stats.img, adjustedCoords(ndx), {width: 10, height: 20}, stats.energyLevel) ]
+        return [...accumulated,new Brick(stats.img, adjustedCoords(ndx), {width: BRICK_WIDTH, height: BRICK_HEIGHT}, stats.energyLevel) ]
     },[] as Brick[] );
 }
 
