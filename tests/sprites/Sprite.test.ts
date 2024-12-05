321ddc9fe40ed09668726585cb26c71a314f918a
Wirt S <34081557+Wirt4@users.noreply.github.com>
2024-12-04 17:38:56 -0800

    tidy sprite test

--- tests/sprites/Sprite.test.ts
+++ tests/sprites/Sprite.test.ts
@@ -2,8 +2,10 @@ import {Sprite} from "../../src/sprites/Sprite";
 
 describe('isInXRange test', () => {
     it('right edge of sprite A touches left edge of sprite B', () => {
-        const spriteA = new Sprite('stub',{x:0,y:0},{width: 5, height:5});
-        const spriteB = new Sprite('stub',{x:5,y:0}, {width: 5, height:5});
-        expect(spriteA.isInXRange(spriteB)).toBe(true);
+        const imageName = 'stub'
+        const spriteSize = {width: 5, height:5}
+        const firstSprite = new Sprite(imageName,{x:0,y:0},spriteSize);
+        const adjacentSprite = new Sprite(imageName,{x:5,y:0},spriteSize);
+        expect(firstSprite.isInXRange(adjacentSprite)).toBe(true);
     })
 })
